e164da850e424dd2b2ae39b7018cc9861f3674f5
WojFar <228730@edu.p.lodz.pl>
2022-10-13 13:38:42 +0200

    copied lab0

---
+++ lab1/source/server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+#print('source code for "http.server":', http.server.__file__)
+
+class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        
+        if self.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()            
+            self.wfile.write(b"Hello World!\n")
+        else:
+            super().do_GET()
+    
+# --- main ---
+
+PORT = 4080
+
+print(f'Starting: http://localhost:{PORT}')
+
+tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server.serve_forever()
